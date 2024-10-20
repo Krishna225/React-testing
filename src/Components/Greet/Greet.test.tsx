20a213b1dddd33b4fa463e7403511a25a3618517
Krishna Theja <krishnatheja.1997@gmal.com>
2024-10-20 17:10:52 +0530

    Pushed changes

--- src/Components/Greet/Greet.test.tsx
+++ src/Components/Greet/Greet.test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from './Greet';
-test('greet renders correctly', ()=>{
-render(<Greet />)
-const textElement= screen.getByText(/hello/i);
-expect(textElement).toBeInTheDocument();
-})
 
-test('greet renders with the name', ()=>{
-    render(<Greet name='Krishna'/>);
-    const textElement= screen.getByText(/hello krishna/i);
-    expect(textElement).toBeInTheDocument();
+describe('Greet', ()=>{
+    test('greet renders correctly', ()=>{
+        render(<Greet />)
+        const textElement= screen.getByText(/Hello/);
+        expect(textElement).toBeInTheDocument();
+        })
+        
+        // test('greet renders with the name', ()=>{
+        //     render(<Greet name='Krishna'/>);
+        //     const textElement= screen.getByText(/hello krishna/i);
+        //     expect(textElement).toBeInTheDocument();
+        // })
 })
---
+++ src/Components/Greet/greet.spec.tsx
@@ -0,0 +1,16 @@
+// import { render, screen } from "@testing-library/react";
+// import Greet from './Greet';
+
+// describe('Greet', ()=>{
+//     it('greet renders correctly', ()=>{
+//         render(<Greet />)
+//         const textElement= screen.getByText(/hello/i);
+//         expect(textElement).toBeInTheDocument();
+//         })
+        
+//         it('greet renders with the name', ()=>{
+//             render(<Greet name='Krishna'/>);
+//             const textElement= screen.getByText(/hello krishna/i);
+//             expect(textElement).toBeInTheDocument();
+//         })
+// })
